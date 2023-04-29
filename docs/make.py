e8e73167e4f121ca4519f0f2b9f6284b9f389960
Christoph Zwerschke <cito@online.de>
2023-04-30 00:02:19 +0200

    Modernize docs make script

--- docs/make.py
+++ docs/make.py
@@ -3,26 +3,27 @@
 """Build HTML from reST files."""
 
 from glob import glob
-from os.path import splitext
+from pathlib import Path
 
 from docutils.core import publish_file
 
 print("Creating the documentation...")
 
 for rst_file in glob('*.rst'):
-    name = splitext(rst_file)[0]
-    lang = splitext(name)[1]
+    rst_path = Path(rst_file)
+    name = Path(rst_file).stem
+    lang = Path(name).suffix
     if lang.startswith('.'):
         lang = lang[1:]
         if lang == 'zh':
             lang = 'zh_cn'
     else:
         lang = 'en'
-    html_file = name + '.html'
+    html_path = Path(name + '.html')
     print(name, lang)
 
-    with open(rst_file, encoding='utf-8-sig') as source:
-        with open(html_file, 'w', encoding='utf-8') as destination:
+    with rst_path.open(encoding='utf-8-sig') as source:
+        with html_path.open('w', encoding='utf-8') as destination:
             output = publish_file(
                 writer_name='html5', source=source, destination=destination,
                 enable_exit_status=True,
